fe8d26608d2ee2403426173fb13415d7ccce2273
Shantanu Acharya <thegeek.004@gmail.com>
2021-11-28 01:48:27 -0500

    training model code added

---
+++ run.py
@@ -0,0 +1,57 @@
+import os
+import argparse
+from tqdm import tqdm
+from stable_baselines3.common.evaluation import evaluate_policy
+from environment.parse_env import make_env, parse_config
+from policy.model import make_model
+
+
+def train_model(model, total_timesteps, save_file):
+    model.learn(total_timesteps)
+    model.save(save_file)
+
+
+def evaluate_model(model, env_name, num_episodes=10):
+    return evaluate_policy(
+        model, make_env(env_name), n_eval_episodes=num_episodes
+    )
+
+
+def test_model(model, env_name, num_episodes=3, record=True):
+    env = make_env(env_name, record=record)
+
+    for episode in tqdm(num_episodes, desc='Test episodes'):
+        obs, done = env.reset(), False
+        while not done:
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, done, info = env.step(action)
+    
+    env.close()
+
+
+if __name__ == '__main__':
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, required=True, help='Environment name')
+    parser.add_argument('-t', '--timesteps', type=int, default=1e6, help='Number of timesteps for training')
+    parser.add_argument('--model', default=os.path.join(BASE_DIR, 'policy', 'dqn.yaml'), help='Model yaml path')
+    parser.add_argument('--weights', default=os.path.join(BASE_DIR, 'weights'), help='Path to store the trained models')
+    parser.add_argument('--logs', default=os.path.join(BASE_DIR, 'logs'), help='Path to store the tensorboard logs')
+    args = parser.parse_args()
+
+    os.makedirs(args.weights, exist_ok=True)
+    os.makedirs(args.logs, exist_ok=True)
+
+    model = make_model(args.model, args.env)
+
+    print('Training model...')
+    train_model(model, int(args.timesteps), os.path.join(args.weights, args.env))
+
+    print('Evaluating model...')
+    mean_reward, std_reward = evaluate_model(model, args.env)
+    print('\n\nMean Reward:', mean_reward)
+    print('Std Reward:', std_reward)
+
+    print('Testing model...')
+    test_model(model, args.env)
